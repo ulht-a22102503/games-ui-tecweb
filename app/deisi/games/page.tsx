472e433b3bb5232dcd7f48f4bb835eae7345bb3c
a22102503 <61268574+ulht-a22102503@users.noreply.github.com>
2025-03-20 20:31:44 +0000

    Update to v2

--- app/deisi/games/page.tsx
+++ app/deisi/games/page.tsx
@@ -5,7 +5,7 @@ import {  Gamepad2 } from 'lucide-react';
 export default function DeisiGamePage() {
     return <div className="w-auto">
         <div className="mx-5 mb-10">
-            <h2 className="text-6xl	font-bold text-center tracking-wide overline decoration-8 ">Games Library</h2>
+            <h2 className="text-6xl	font-bold text-center tracking-wide overline decoration-8 ">Games Library v2</h2>
             
             <h3 className="flex align-center  space-x-2 text-xl mt-5 font-bold text-center" id="products">
                 <Gamepad2 className="h-6 w-6 inline-block mr-2" />
